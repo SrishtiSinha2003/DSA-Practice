35e96f0ce060758b9c93fbf2fa1e7bceb31229d2
Srishti Sinha <srishtiisinha2003@gmail.com>
2025-12-15 21:05:26 +0530

    added files

---
+++ bitManipulation/duplicateXOR.java
@@ -0,0 +1,22 @@
+public class duplicateXOR {
+    public static int duplicateNumbersXOR(int[] nums) {
+        int[] freq = new int[51]; // because nums[i] <= 50
+
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        int xor = 0;
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] == 2) {
+                xor ^= i;
+            }
+        }
+
+        return xor;
+    }
+    public static void main(String[] args) {
+        int[] num = {1,2,2,1};
+        System.out.println(duplicateNumbersXOR(num));
+    }
+}
