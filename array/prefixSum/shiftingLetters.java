45b32f481c063a9ec36a0aed425a9db514845e6e
Srishti Sinha <srishtiisinha2003@gmail.com>
2025-12-17 19:51:58 +0530

    added shifting-char code

--- array/prefixSum/shiftingLetters.java
+++ array/prefixSum/shiftingLetters.java
@@ -1,3 +1,20 @@
 public class shiftingLetters {
-    
+    public static String shift(int[] shifts, String s) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+
+        int totalShift = 0;
+
+        for(int i=n-1; i>=0; i--) {
+            totalShift = (totalShift + shifts[i]) % 26;
+            int shiftedChar = (arr[i] - 'a' + (int) totalShift) % 26;
+            arr[i] = (char) ('a' + shiftedChar);
+        }
+        return new String(arr);
+    }
+    public static void main(String args[]) {
+        String s = "abc";
+        int[] shifts = {3,5,9};
+        System.out.println(shift(shifts, s));
+    }
 }
