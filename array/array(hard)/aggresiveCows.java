033dbd791b76a51c94e1833e54a443e7826e432d
Srishti Sinha <srishtiisinha2003@gmail.com>
2025-12-17 07:54:07 +0530

    added code for aggressive cow

--- array/array(hard)/aggresiveCows.java
+++ array/array(hard)/aggresiveCows.java
@@ -1,3 +1,39 @@
+import java.util.Arrays;
 public class aggresiveCows {
-    
+    public static boolean check(int[] stalls, int k, int dist) {
+        int cnt = 1;
+        int prev = stalls[0];
+
+        for(int i=1; i<stalls.length; i++) {
+            if(stalls[i] - prev >= dist) {
+                cnt++;
+                prev = stalls[i];
+            }
+        }
+        return ( cnt >= k);
+    }
+    public static int cows(int[] stalls, int k) {
+        Arrays.sort(stalls);
+        int res = 0; // to keep best possible result
+
+        int l = 1;
+        int h = stalls[stalls.length - 1] - stalls[0];
+
+        while(l <= h) {
+            int mid = l + (h - l)/2;
+            if(check(stalls, k, mid)) {
+                res = mid;
+                l = mid + 1;
+            }
+            else {
+                h = mid -1;
+            }
+        }
+        return res;
+    }
+    public static void main (String[] arrgs) {
+        int[] stalls = {1,2,4,8,9};
+        int k = 3;
+        System.out.println(cows(stalls, k));
+    }
 }
