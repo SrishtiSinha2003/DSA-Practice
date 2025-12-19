2fc3eb0b18ef4a3a65b01d89ca84037c201aa4f8
Srishti Sinha <srishtiisinha2003@gmail.com>
2025-12-20 00:00:32 +0530

    added file

---
+++ array/two-pointers/buyAndSellStocksMultiTransac.java
@@ -0,0 +1,15 @@
+public class buyAndSellStocksMultiTransac {
+    public static int profit(int[] prices) {
+        int res = 0;
+        for(int i=1; i<prices.length; i++) {
+            if(prices[i] > prices[i-1]) {
+                res += prices[i] - prices[i-1];
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int[] prices = {100, 180, 260, 310, 40, 535, 695};
+        System.out.println(profit(prices));
+    }
+}
