f19dc4699650589653a261f2df72b4d0f58a9f03
Srishti Sinha <srishtiisinha2003@gmail.com>
2025-12-20 19:20:27 +0530

    added missing positive code

---
+++ array/array(medium)/smallestPositiveMissing.java
@@ -0,0 +1,24 @@
+//cycle sort O(n), O(1)
+public class smallestPositiveMissing {
+    public static int missing(int[] arr) {
+        int n = arr.length;
+        for(int i=0; i<n; i++) {
+            while(arr[i] >= 1 && arr[i] <= n
+                && arr[i] != arr[arr[i]-1]) {
+                int temp = arr[i];
+                arr[i] = arr[arr[i]-1];  
+                arr[temp - 1] = temp;
+            }
+        }
+        for(int i=1; i<=n; i++) {
+            if(i != arr[i-1]) {
+                return i;
+            }
+        }
+        return n+1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2, -3, 4, 1, 1, 7};
+        System.out.println(missing(arr));
+    }
+}
