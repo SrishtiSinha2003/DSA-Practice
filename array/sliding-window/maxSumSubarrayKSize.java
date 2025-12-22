a0d297e4b0d1cb2fda35ad3a5784bb7e9ff634eb
Srishti Sinha <srishtiisinha2003@gmail.com>
2025-12-22 08:51:38 +0530

    added file

---
+++ array/sliding-window/maxSumSubarrayKSize.java
@@ -0,0 +1,28 @@
+public class maxSumSubarrayKSize {
+    static int maxSubarraySum(int[] arr, int k) {
+        int n = arr.length;
+        if (n < k) return -1;
+
+        // compute sum of first window
+        int windowSum = 0;
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+
+        int maxSum = windowSum;
+
+        // slide the window
+        for (int i = k; i < n; i++) {
+            windowSum += arr[i] - arr[i - k];
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 5, 1, 3, 2};
+        int k = 3;
+        System.out.println(maxSubarraySum(arr, k));
+    }
+}
