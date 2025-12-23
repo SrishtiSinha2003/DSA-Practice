b2f00044d40d324bd8a34a7db66588fba1e26309
Srishti Sinha <srishtiisinha2003@gmail.com>
2025-12-23 15:24:45 +0530

    added file

---
+++ strings/sentencePalindrome.java
@@ -0,0 +1,22 @@
+public class sentencePalindrome {
+    public static boolean isPalin(String s) {
+        int i=0, j = s.length()-1;
+        while(i < j) {
+            if(!Character.isLetterOrDigit(s.charAt(i)))
+                i++;
+            else if(!!Character.isLetterOrDigit(s.charAt(j)))
+                j--;
+            else if(Character.toLowerCase(s.charAt(i)) == Character.toLowerCase(s.charAt(j))) {
+                i++;
+                j--;
+            }
+            else 
+                return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String s = "ABC$.def01ASDF..";
+        System.out.println(isPalin(s));
+    }
+}
