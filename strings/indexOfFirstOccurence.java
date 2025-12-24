c3d6d62ed76357764920e9a8c046b78d5575a37f
Srishti Sinha <srishtiisinha2003@gmail.com>
2025-12-24 16:15:46 +0530

    added file

---
+++ strings/indexOfFirstOccurence.java
@@ -0,0 +1,25 @@
+public class indexOfFirstOccurence {
+    public static int strStr(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            int j = 0;
+
+            while (j < needle.length() &&
+                   haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+
+            if (j == needle.length()) {
+                return i; // full match found
+            }
+        }
+
+        return -1; // no match
+    }
+    public static void main(String[] args) {
+        String haystack = "sadbutsad";
+        String needle = "sad";
+        System.out.println(strStr(haystack,needle));
+    }
+}
