c8663c2643aa26e2be47ba38b2ee4e43536b6b14
Srishti Sinha <srishtiisinha2003@gmail.com>
2025-12-26 15:31:23 +0530

    added file

---
+++ array/array(hard)/kthMissing.java
@@ -0,0 +1,23 @@
+public class kthMissing {
+    public static int missing(int[] arr, int k) {
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int missing = arr[mid] - (mid + 1);
+
+            if (missing < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left + k;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 7, 11};
+        int k = 5;
+        System.out.println(missing(arr, k));
+    } 
+}
