f8290d3d63e906a64f82e218f3dee03af96073e1
Srishti Sinha <srishtiisinha2003@gmail.com>
2025-12-29 16:04:16 +0530

    added files

---
+++ array/array(easy)/checkIfArrayIsSortedAndRotated.java
@@ -0,0 +1,20 @@
+public class checkIfArrayIsSortedAndRotated {
+    public static boolean check(int[] nums) {
+        // here we compare all the neighbouring elemnts and check whether they are in somewhat sorted
+        // there will be a small change due to rotation in the array at only one place.
+        // so if there are irregularities more than once, return false
+        // else return true;
+        int irregularities = 0;
+        int length = nums.length;
+        for (int i=0; i<length; i++) {
+            if (nums[i] > nums[(i + 1) % length])
+                irregularities += 1;
+        }
+        return irregularities > 1 ? false : true;
+    }
+    public static void main(String[] args) {
+        int[] nums = {3,4,5,1,2};
+        System.out.println(check(nums));
+    }
+}
+
