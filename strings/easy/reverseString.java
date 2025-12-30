4dcb93e260ca2e712d77deaefcedda6adb9f5bd0
Srishti Sinha <srishtiisinha2003@gmail.com>
2025-12-30 15:57:30 +0530

    added file

--- strings/easy/reverseString.java
+++ strings/easy/reverseString.java
@@ -18,3 +18,26 @@ public class reverseString {
         System.out.println(Arrays.toString(s));
     }
 }
+
+/*// User function Template for Java
+
+class Solution {
+    public static String reverseString(String s) {
+        // code here
+        char[] arr = s.toCharArray();
+        int i = 0, j = arr.length - 1;
+
+        while (i < j) {
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+
+        return new String(arr);
+    }
+}
+
+    
+ */
