cac9e706c645f4b89c029ee34f28f924669b7064
Srishti Sinha <srishtiisinha2003@gmail.com>
2026-01-05 15:51:13 +0530

    added file

---
+++ strings/medium/reverseWordsInString.java
@@ -0,0 +1,17 @@
+public class reverseWordsInString {
+    public static String reverse(String s) {
+        String[] words = s.trim().split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for(int i=words.length-1; i>=0; i--) {
+            sb.append(words[i]);
+            if(i != 0) {
+                sb.append(" ");
+            }
+        }   
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+        System.out.println(reverse(s));
+    }
+}
