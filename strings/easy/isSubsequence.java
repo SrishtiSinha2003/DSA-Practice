1525d4341970fb4caf46a96e1f4fe6e6582468c8
Srishti Sinha <srishtiisinha2003@gmail.com>
2026-01-07 16:48:52 +0530

    added file

---
+++ strings/easy/isSubsequence.java
@@ -0,0 +1,17 @@
+public interface isSubsequence {
+    public static boolean isSub(String s, String t) {
+        int i=0, j=0;
+        while (i < s.length() && j < t.length()) {
+            if(s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
+    }
+    public static void main(String[] args) {
+        String s = "axc";
+        String t = "ahbgdc";
+        System.out.println(isSub(s, t));
+    }
+}
