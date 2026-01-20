b09b3b2fdadf3db6a3d2b5b9e2d8e3aeebe8ca05
Srishti Sinha <srishtiisinha2003@gmail.com>
2026-01-20 19:56:58 +0530

    added files

--- linkedList/insertFront.java
+++ linkedList/insertFront.java
@@ -1,3 +1,34 @@
 public class insertFront {
-    
+    static class Node{
+        int value;
+        Node next;
+
+        public Node(int val) {
+            value = val;
+            next = null;
+        }
+    }
+    static class LinkedList {
+        public Node head;
+            public void insert(int value) {
+            System.out.println("Inserting " + value);
+            Node newNode = new Node(value);
+            newNode.next = head;
+            head = newNode;
+        }
+        public int getHeadValue(){
+            if(head == null) {
+                return -1;
+            } else {
+                return head.value;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insert(3);
+        System.out.println(list.getHeadValue());
+        list.insert(2);
+        System.out.println(list.getHeadValue());
+    }
 }
