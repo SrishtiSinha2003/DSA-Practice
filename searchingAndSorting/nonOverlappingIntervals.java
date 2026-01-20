d224317a74efdc56c5a81d9c33d0350bea41e20f
Srishti Sinha <srishtiisinha2003@gmail.com>
2026-01-20 23:14:31 +0530

    added file

---
+++ searchingAndSorting/nonOverlappingIntervals.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class nonOverlappingIntervals {  
+    public static int overlapping(int[][] intervals) {
+        int count = 0;
+        Arrays.sort(intervals, (a,b)-> a[1]-b[1]);
+        int end = intervals[0][1];
+        for(int i=1; i<intervals.length; i++) {
+            if(intervals[i][0] < end) {
+                count++;
+            } else {
+                end = intervals[i][1];
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[][] intervals = {{1,2}, {2,3}, {3,4},{1,3}};
+        System.out.println(overlapping(intervals));
+    }
+}
