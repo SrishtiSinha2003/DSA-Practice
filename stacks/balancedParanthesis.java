4a9f71ffd945b1ad19f7ac7b976d4af03289d59b
Srishti Sinha <srishtiisinha2003@gmail.com>
2026-01-29 20:50:49 +0530

    added files

---
+++ stacks/balancedParanthesis.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class balancedParanthesis {
+    public static boolean isBalanced(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()) {
+            if(c == '(' || c == '{' || c == '[') {
+            stack.push(c);
+            } else {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                char ch = stack.pop();
+                if((c == ')' && ch != '(') || (c == '{' 
+                && ch != '}') || (c == '[' && ch != ']'))
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        String s = "{{(())}[]";
+        System.out.println(isBalanced(s));
+    }
+}
