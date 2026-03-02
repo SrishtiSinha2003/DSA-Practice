3ef8abec0080afa2e67ce13c05cf7ee96382fcb1
Srishti Sinha <srishtiisinha2003@gmail.com>
2026-03-02 22:24:40 +0530

    added sort character by frequency solution

---
+++ Revision/sortCharByFreq.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class sortCharByFreq {
+    public static String sort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>(
+            (a,b) -> b.getValue()-a.getValue());
+            pq.addAll(map.entrySet());
+
+            StringBuilder sb = new StringBuilder();
+            while (!pq.isEmpty()) {
+                Map.Entry<Character, Integer> entry = pq.poll();
+                sb.append(String.valueOf(entry.getKey()).repeat(entry.getValue()));
+            }
+            return sb.toString();
+    }
+    public static void main(String[] args) {
+        String s = "tree";
+        System.out.println(sort(s));
+    }
+}
