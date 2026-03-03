0abb14b7540b4e3c82d484351f3ba878ddc9b3c0
Srishti Sinha <srishtiisinha2003@gmail.com>
2026-03-03 17:10:46 +0530

    added file

---
+++ Revision/validAnagram.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class validAnagram{
+    public static boolean valid(String s, String t) {
+        if(s.length() != t.length()) return false;
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        return Arrays.equals(sArray, tArray);
+    }
+    public static void main(String[] args) {
+       String s = "geeks";
+       String t = "kseeg";
+       System.out.println(valid(s, t));
+    }
+}
