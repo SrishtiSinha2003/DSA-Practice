537d4978e59c03a82b429595987882d0147128be
Srishti Sinha <srishtiisinha2003@gmail.com>
2026-03-03 17:17:56 +0530

    added file

---
+++ Revision/validParanthesis.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class validParanthesis {
+    public static boolean isBalanced(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()) {
+            if(c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if(stack.isEmpty()) return false;
+                char ch = stack.pop();
+                if((c == ')' && ch != '(') || (c == '{' 
+                && ch != '}') || (c == '[' && ch != ']'))
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+     public static void main(String[] args) {
+        String s = "{{(())}}[]";
+        System.out.println(isBalanced(s));
+    }
+}
