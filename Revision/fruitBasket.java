b2c6c6dbc798d44906e30bbdd8d7405b47e1e038
Srishti Sinha <srishtiisinha2003@gmail.com>
2026-03-03 17:04:29 +0530

    added fruitBasket.java

---
+++ Revision/fruitBasket.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class fruitBasket {
+    public static int distinct(int[] arr) {
+        int left = 0;
+        int maxLength = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int right = 0; right < arr.length; right++) {
+            map.put(arr[right], map.getOrDefault(arr[right], 0)+1);
+            while(map.size() > 2) {
+                map.put(arr[left], map.get(arr[left])-1);
+                if(map.get(arr[left]) == 0) {
+                    map.remove(arr[left]);
+                }
+                left++;
+            }
+            maxLength = Math.max(maxLength, right-left+1);
+        }
+        return maxLength;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,1};
+        System.out.print(distinct(arr)); 
+    }
+}
