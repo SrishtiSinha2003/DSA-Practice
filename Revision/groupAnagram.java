eb0a9a3d34d9ae394479f747eac84ee21f75a24a
Srishti Sinha <srishtiisinha2003@gmail.com>
2026-03-09 10:37:42 +0530

    added file

--- Revision/groupAnagram.java
+++ Revision/groupAnagram.java
@@ -1,11 +1,10 @@
 import java.util.*;
-public class groupAnagrams {
+public class groupAnagram {
     public static List<List<String>> anagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
-
-        for (String s : strs) {
+        for(String s : strs) {
             char[] arr = s.toCharArray();
-            Arrays.sort(arr);                 // create signature
+            Arrays.sort(arr);
             String key = new String(arr);
 
             if(!map.containsKey(key)) {
@@ -16,12 +15,9 @@ public class groupAnagrams {
 
         return new ArrayList<>(map.values());
     }
-
     public static void main(String[] args) {
         String[] strs = {"eat","tea","tan","ate","nat","bat"};
         List<List<String>> result = anagrams(strs);
-
-        // Print the result
         for (List<String> group : result) {
             System.out.println(group);
         }
