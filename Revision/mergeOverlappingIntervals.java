de4c16e4037ad041e10939fba9953a8b5d0fafde
Srishti Sinha <srishtiisinha2003@gmail.com>
2026-03-11 12:15:31 +0530

    added file

---
+++ Revision/mergeOverlappingIntervals.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class mergeOverlappingIntervals {
+    public static ArrayList<int[]> mergeOverlap(int[][] arr) {
+        Arrays.sort(arr, (a,b) -> Integer.compare(a[0], b[0]));
+        ArrayList<int[]> res = new ArrayList<>();
+        res.add(new int[]{arr[0][0], arr[0][1]});
+        for(int i=1; i<arr.length; i++) {
+            int[] last = res.get(res.size()-1);
+            int[] curr = arr[i];
+
+            if(curr[0] <= last[1]) {
+                last[1] = Math.max(last[1], curr[1]); 
+            } else {
+                res.add(new int[]{curr[0], curr[1]});
+            }
+        }
+        return res;
+    }
+     public static void main(String[] args) {
+        int[][] arr = {{4,7},{1,4}};
+        ArrayList<int[]> res = mergeOverlap(arr);
+
+        for (int[] interval : res)
+            System.out.println(interval[0] + " " + interval[1]);
+    }
+}
