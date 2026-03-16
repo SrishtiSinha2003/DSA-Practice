cf022b32868b3ec9fdc79cafaee6f59271fadf1c
Srishti Sinha <srishtiisinha2003@gmail.com>
2026-03-16 16:38:49 +0530

    added file

---
+++ Revision/reverseLinkedList.java
@@ -0,0 +1,49 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val  = val;
+        this.next = null;
+    }
+}
+public class reverseLinkedList {
+    public static ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+     public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        System.out.print("Original List: ");
+        printList(head);
+
+        head = reverse(head);
+
+        System.out.print("Reversed List: ");
+        printList(head);
+    }
+}
